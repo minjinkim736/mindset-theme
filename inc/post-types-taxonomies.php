d30099d4954a8a60cb52e8b4432c6a1b751458e7
Minjin Kim <0sthabtus0@gmail.com>
2025-01-31 17:24:16 -0800

    finished day06 afternoon task

--- inc/post-types-taxonomies.php
+++ inc/post-types-taxonomies.php
@@ -98,7 +98,111 @@ function mindset_register_custom_post_types()
     );
 
     register_post_type('fwd-testimonial', $args);
+
+    // job posts CPT
+    $labels = array(
+        'name'                  => _x('Job Postings', 'post type general name', 'mindset-theme'),
+        'singular_name'         => _x('Job Posting', 'post type singular name', 'mindset-theme'),
+        'menu_name'             => _x('Job Postings', 'admin menu', 'mindset-theme'),
+        'add_new'               => _x('Add New', 'service', 'mindset-theme'),
+        'add_new_item'          => __('Add New Job Posting', 'mindset-theme'),
+        'new_item'              => __('New Job Posting', 'mindset-theme'),
+        'edit_item'             => __('Edit Job Posting', 'mindset-theme'),
+        'view_item'             => __('View Job Posting', 'mindset-theme'),
+        'all_items'             => __('All Job Postings', 'mindset-theme'),
+        'search_items'          => __('Search Job Postings', 'mindset-theme'),
+        'parent_item_colon'     => __('Parent Job Postings:', 'mindset-theme'),
+        'not_found'             => __('No Job Postings found.', 'mindset-theme'),
+        'not_found_in_trash'    => __('No Job Postings found in Trash.', 'mindset-theme'),
+        'insert_into_item'      => __('Insert into Job Posting', 'mindset-theme'),
+        'uploaded_to_this_item' => __('Uploaded to this Job Posting', 'mindset-theme'),
+        'item_link'             => __('Job Posting link.', 'mindset-theme'),
+        'item_link_description' => __('A link to a job posting.', 'mindset-theme'),
+    );
+    $args = array(
+        'labels'             => $labels,
+        'public'             => true,
+        'publicly_queryable' => true,
+        'show_ui'            => true,
+        'show_in_menu'       => true,
+        'show_in_rest'       => true,
+        'query_var'          => true,
+        'rewrite'            => array('slug' => 'careers'),
+        'capability_type'    => 'post',
+        'has_archive'        => true,
+        'hierarchical'       => false,
+        'menu_position'      => 21,
+        'menu_icon'          => 'dashicons-megaphone',
+        'supports'           => array('title', 'editor'),
+        'template'           => array(
+            array('core/heading', array('level' => 3, 'content' => 'Role',)),
+            array('core/paragraph', array('placeholder' => 'Describe the role...')),
+            array('core/heading', array('level' => 3, 'content' => 'Requirements')),
+            array('core/list'),
+            array('core/heading', array('level' => 3, 'content' => 'Location')),
+            array('core/paragraph'),
+            array('core/heading', array('level' => 3, 'content' => 'How to Apply')),
+            array('core/paragraph'),
+        )
+    );
+    register_post_type('fwd-job-posting', $args);
+
+    // services CPT
+    $labels = array(
+        'name' => _x('Services', 'Post Type General Name', 'mindset-theme'),
+        'singular_name' => _x('Service', 'Post Type Singular Name', 'mindset-theme'),
+        'menu_name' => _x('Services', 'Admin Menu text', 'mindset-theme'),
+        'name_admin_bar' => _x('Service', 'Add New on Toolbar', 'mindset-theme'),
+        'archives' => __('Service Archives', 'mindset-theme'),
+        'attributes' => __('Service Attributes', 'mindset-theme'),
+        'parent_item_colon' => __('Parent Service:', 'mindset-theme'),
+        'all_items' => __('All Services', 'mindset-theme'),
+        'add_new_item' => __('Add New Service', 'mindset-theme'),
+        'add_new' => __('Add New', 'mindset-theme'),
+        'new_item' => __('New Service', 'mindset-theme'),
+        'edit_item' => __('Edit Service', 'mindset-theme'),
+        'update_item' => __('Update Service', 'mindset-theme'),
+        'view_item' => __('View Service', 'mindset-theme'),
+        'view_items' => __('View Services', 'mindset-theme'),
+        'search_items' => __('Search Service', 'mindset-theme'),
+        'not_found' => __('Not found', 'mindset-theme'),
+        'not_found_in_trash' => __('Not found in Trash', 'mindset-theme'),
+        'featured_image' => __('Featured Image', 'mindset-theme'),
+        'set_featured_image' => __('Set featured image', 'mindset-theme'),
+        'remove_featured_image' => __('Remove featured image', 'mindset-theme'),
+        'use_featured_image' => __('Use as featured image', 'mindset-theme'),
+        'insert_into_item' => __('Insert into Service', 'mindset-theme'),
+        'uploaded_to_this_item' => __('Uploaded to this Service', 'mindset-theme'),
+        'items_list' => __('Services list', 'mindset-theme'),
+        'items_list_navigation' => __('Services list navigation', 'mindset-theme'),
+        'filter_items_list' => __('Filter Services list', 'mindset-theme'),
+    );
+    $args = array(
+        'label' => __('Service', 'mindset-theme'),
+        'description' => __('', 'mindset-theme'),
+        'labels' => $labels,
+        'menu_icon' => 'dashicons-buddicons-topics',
+        'supports' => array('title', 'editor'),
+        'taxonomies' => array(),
+        'public' => true,
+        'show_ui' => true,
+        'show_in_menu' => true,
+        'menu_position' => 5,
+        'show_in_admin_bar' => true,
+        'show_in_nav_menus' => true,
+        'can_export' => true,
+        'has_archive' => false,
+        'hierarchical' => false,
+        'exclude_from_search' => false,
+        'show_in_rest' => true,
+        'publicly_queryable' => true,
+        'capability_type' => 'post',
+    );
+    register_post_type('service', $args);
+
+    add_action('init', 'create_service_cpt', 0);
 }
+
 add_action('init', 'mindset_register_custom_post_types');
 
 function mindset_rewrite_flush()
--- mindset-blocks/mindset-blocks.php
+++ mindset-blocks/mindset-blocks.php
@@ -1,7 +1,7 @@
 <?php
 
- 
-if ( ! defined( 'ABSPATH' ) ) {
+
+if (! defined('ABSPATH')) {
 	exit; // Exit if accessed directly.
 }
 
@@ -12,19 +12,22 @@ if ( ! defined( 'ABSPATH' ) ) {
  *
  * @see https://developer.wordpress.org/reference/functions/register_block_type/
  */
-function mindset_blocks_mindset_blocks_block_init() {
-	register_block_type( __DIR__ . '/build/copyright' );
-	register_block_type( __DIR__ . '/build/company-address' ); //folder name
-	register_block_type( __DIR__ . '/build/company-email' ); //folder name
+function mindset_blocks_mindset_blocks_block_init()
+{
+	register_block_type(__DIR__ . '/build/copyright');
+	register_block_type(__DIR__ . '/build/company-address'); //folder name
+	register_block_type(__DIR__ . '/build/company-email'); //folder name
+	register_block_type(__DIR__ . '/build/service-posts', array('render_callback' => 'fwd_render_service_posts')); //folder name
 }
-add_action( 'init', 'mindset_blocks_mindset_blocks_block_init' );
+add_action('init', 'mindset_blocks_mindset_blocks_block_init');
 
 /**
-* Registers the custom fields for some blocks.
-*
-* @see https://developer.wordpress.org/reference/functions/register_post_meta/
-*/
-function mindset_register_custom_fields() {
+ * Registers the custom fields for some blocks.
+ *
+ * @see https://developer.wordpress.org/reference/functions/register_post_meta/
+ */
+function mindset_register_custom_fields()
+{
 	register_post_meta(
 		'page',
 		'company_email',
@@ -44,4 +47,58 @@ function mindset_register_custom_fields() {
 		)
 	);
 }
-add_action( 'init', 'mindset_register_custom_fields' );
+add_action('init', 'mindset_register_custom_fields');
+
+// service posts
+function fwd_render_service_posts($attributes) {
+	ob_start();
+?>
+	<div <?php echo get_block_wrapper_attributes(); ?>>
+		<?php
+		// Add a WP_Query() custom loop here
+		// See slides for details
+		// query for service posts
+
+		//copy of WP_Qeury() custom loop for navigation
+		$args_nav = array(
+			'post_type'      => 'service',
+			'posts_per_page' => -1,
+			'order'          => 'ASC',
+		);
+
+		$query_nav = new WP_Query($args_nav);
+
+		if ($query_nav->have_posts()) {
+			echo '<nav class="service-nav">';
+			while ($query_nav->have_posts()) {
+				$query_nav->the_post();
+				echo '<a href="#post-' . get_the_ID() . '">' . get_the_title() . '</a>';
+			}
+			echo '</nav>';
+			wp_reset_postdata();
+		}
+
+		$args = array(
+			'post_type'      => 'service',
+			'posts_per_page' => -1,
+			'order'          => 'ASC',
+		);
+
+		$query = new WP_Query($args);
+		if ($query->have_posts()) {
+			while ($query->have_posts()) {
+				$query->the_post();
+
+				echo '<article id="post-' . get_the_ID() . '">';
+				echo '<h2>' . get_the_title() . '</h2>'; // Title in h2
+				the_content(); // Content from block editor
+				echo '</article>';
+			}
+			wp_reset_postdata();
+		}
+
+		?>
+	</div>
+<?php
+	return ob_get_clean();
+}
